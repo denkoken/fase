408679955e090ee095b5ec1d9e40acf6ee52a4f9
takiyu <takiyu1025.txu.development@gmail.com>
2018-06-21 02:47:01 +0900

    Add static assert and comment.

--- src/fase.h
+++ src/fase.h
@@ -73,25 +73,44 @@ public:
 template <typename... Args>
 class StandardFunction : public FunctionNode {
 public:
+
+    ///
+    /// Wrap a function which dose not return anything.
+    ///
     template <class Callable>
     StandardFunction(Callable in_func) {
         func = std::function<void(Args...)>(in_func);
     }
 
+    ///
+    /// Build function with input arguments statically.
+    ///
     template <typename... VArgs>
     FunctionNode *build(VArgs&... in_args) {
-        args = std::array<Variable, sizeof...(VArgs)>{(in_args)...};
+        static_assert(sizeof...(VArgs) == sizeof...(Args),
+                      "The number of arguments is not matched");
+
+        // Copy to an array of variables
+        args = std::array<Variable, sizeof...(Args)>{in_args...};
+        // Bind arguments
         binded = bind(func, std::index_sequence_for<Args...>());
+
         return this;
     }
 
+    ///
+    /// Build function with input arguments dynamically.
+    ///
     FunctionNode *build(const std::vector<Variable *> &in_args) {
         if (in_args.size() != sizeof...(Args)) {
             throw(InvalidArgN(sizeof...(Args), in_args.size()));
         }
+
+        // Copy to an array of variables
         for (int i = 0; i < int(sizeof...(Args)); i++) {
             args[i] = *in_args[i];
         }
+        // Bind arguments
         binded = bind(func, std::index_sequence_for<Args...>());
 
         return this;
