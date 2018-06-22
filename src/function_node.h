7cfefe4fc8b5a5327cbd15a8fe53a4d97c7fd425
skatto <skattomaskatto@gmail.com>
2018-06-22 16:08:51 +0900

    add FunctionBuilder and FunctionBinder

--- src/function_node.h
+++ src/function_node.h
@@ -83,6 +83,69 @@ private:
     std::array<Variable, sizeof...(Args)> args;
 };
 
+class FunctionBuilder {
+public:
+    virtual std::function<void()> build(const std::vector<Variable *> &in_args) = 0;
+};
+
+template <typename... Args>
+class FunctionBinder : public FunctionBuilder {
+public:
+    ///
+    /// Wrap a function which dose not return anything.
+    ///
+    template <class Callable>
+    FunctionBinder(Callable in_func) {
+        func = std::function<void(Args...)>(in_func);
+    }
+
+    ///
+    /// Build function with input arguments statically.
+    ///  TODO: Is there any method to replace template VArgs with Args?
+    ///
+    template <typename... VArgs>
+    std::function<void()> build(VArgs&... in_args) noexcept {
+        static_assert(sizeof...(VArgs) == sizeof...(Args),
+                      "The number of arguments is not matched");
+
+        // Copy to an array of variables
+        args = std::array<Variable, sizeof...(Args)>{{in_args...}};
+        // Bind arguments
+        return bind(std::index_sequence_for<Args...>());
+    }
+
+    ///
+    /// Build function with input arguments dynamically.
+    ///
+    std::function<void()> build(const std::vector<Variable *> &in_args) {
+        if (in_args.size() != sizeof...(Args)) {
+            throw(InvalidArgN(sizeof...(Args), in_args.size()));
+        }
+
+        // Copy to an array of variables
+        for (size_t i = 0; i < sizeof...(Args); i++) {
+            args[i] = *in_args[i];
+        }
+        // Bind arguments
+        return bind(std::index_sequence_for<Args...>());
+
+        return this;
+    }
+
+private:
+    template <std::size_t... Idx>
+    auto bind(std::index_sequence<Idx...>) {
+        return [&]() {
+            func(*args[Idx]
+                   .template getReader<
+                       typename std::remove_reference<Args>::type>()...);
+        };
+    }
+
+    std::function<void(Args...)> func;
+    std::array<Variable, sizeof...(Args)> args;
+};
+
 }  // namespace fase
 
 #endif  // FASE_FUNCTION_NODE_H_20180617
