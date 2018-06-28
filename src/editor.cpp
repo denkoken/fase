98f68ad10058efd5edaba0843f4192120e8e67e2
skatto <skattomaskatto@gmail.com>
2018-06-28 20:47:54 +0900

    write CLIEditor's main system

--- src/editor.cpp
+++ src/editor.cpp
@@ -3,8 +3,19 @@
 
 #include <iostream>
 #include <sstream>
+#include <map>
 
 namespace {
+
+template <typename T, typename S>
+inline bool exists(const std::map<T, S>& map, const T& key) {
+    return map.find(key) != std::end(map);
+}
+
+template <class T, typename S>
+inline bool exists(const T& list, const S& key) {
+    return std::find(std::begin(list), std::end(list), key) != std::end(list);
+}
 
 std::vector<std::string> split(const std::string& str, const char& sep) {
     std::vector<std::string> dst;
@@ -16,6 +27,18 @@ std::vector<std::string> split(const std::string& str, const char& sep) {
     return dst;
 }
 
+std::vector<std::string> clInput() {
+    std::cout << " > ";
+    std::string input_str;
+    std::getline(std::cin, input_str);
+
+    return split(input_str, ' ');
+}
+
+void add(fase::pe::FaseCore* core, std::vector<std::string> input) {
+    std::cout << "add" << std::endl;
+}
+
 }  // namespace
 
 namespace fase {
@@ -23,17 +46,28 @@ namespace fase {
 namespace editor {
 
 void CLIEditor::start(pe::FaseCore* core, Variable variable) {
+    using std::string;
+    using Command = std::function<void(pe::FaseCore*, std::vector<string>)>;
+
+    const string exit_commands[] = {"quit", "exit", "logout"};
+
+    std::map<string, Command> commands;
+
+    commands["add"] = add;
+
     while (true) {
-        std::cout << " > ";
-        std::string input_str;
-        std::getline(std::cin, input_str);
-        std::cout << input_str << std::endl;
+        std::vector<string> input = clInput();
 
-        std::vector<std::string> input = split(input_str, ' ');
-
-        if (input[0] == std::string("quit")) {
+        if (exists(exit_commands, input[0])) {
             break;
         }
+
+        if (!exists(commands, input[0])) {
+            std::cout << "Undefined Command : "<< input[0] << std::endl;
+            continue;
+        }
+
+        commands[input[0]](core, input);
     }
 }
 
