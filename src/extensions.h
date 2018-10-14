51cf306b8f15c974e45e12a178b6fd341d941ac3
skatto <skattomaskatto@gmail.com>
2018-10-14 23:38:44 +0900

    add DirectlyAccessibleBareCore Parts

---
+++ src/extensions.h
@@ -0,0 +1,19 @@
+
+#ifndef EXTENSIONS_H_20181014
+#define EXTENSIONS_H_20181014
+
+#include <memory>
+
+#include "core.h"
+#include "parts_base.h"
+
+namespace fase {
+
+class DirectlyAccessibleBareCore : public PartsBase {
+public:
+    std::shared_ptr<FaseCore> getAccessibleCore() { return getCore(); }
+};
+
+}
+
+#endif // EXTENSIONS_H_20181014
