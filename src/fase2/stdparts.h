379c15ff13ce220d0d2f3c71f89b5855967f518b
skatto <skattomaskatto@gmail.com>
2019-03-18 23:57:42 +0900

    refactoring

--- src/fase2/stdparts.h
+++ src/fase2/stdparts.h
@@ -6,14 +6,15 @@
 
 #include "parts_base.h"
 
+// =============================================================================
+// =========================== User Interface ==================================
+// =============================================================================
+
 namespace fase {
 
 class ExportableParts : public PartsBase {
 public:
-    ExportedPipe exportPipe() const {
-        auto [guard, pcm] = getReader();
-        return pcm->exportPipe(pcm->getFocusedPipeline());
-    }
+    inline ExportedPipe exportPipe() const;
 };
 
 template <typename... RetTypes>
@@ -23,34 +24,7 @@ public:
     class Pipe {
     public:
         template <class Exported>
-        static auto Gen(Exported&& exported) {
-            class Dst {
-            public:
-                std::tuple<RetTypes...> operator()(Args... args) {
-                    std::vector<Variable> arg_vs = {
-                            std::make_unique<std::decay_t<Args>>(
-                                    std::forward<Args>(args))...};
-                    [[maybe_unused]] auto _ = {
-                            arg_vs.emplace_back(typeid(RetTypes))...};
-                    if (!soft(arg_vs)) {
-                        throw std::runtime_error(
-                                "HardExportPipe : input/output type isn't "
-                                "match!");
-                    }
-                    std::vector<Variable*> ret_ps;
-                    for (std::size_t i = arg_vs.size() - sizeof...(RetTypes);
-                         i < arg_vs.size(); i++) {
-                        ret_ps.emplace_back(&arg_vs[i]);
-                    }
-                    return Wrap(ret_ps, std::index_sequence_for<RetTypes...>());
-                }
-                void reset() {
-                    soft.reset();
-                }
-                std::decay_t<Exported> soft;
-            };
-            return Dst{std::forward<Exported>(exported)};
-        }
+        static inline auto Gen(Exported&& exported);
     };
 
 private:
@@ -64,34 +38,90 @@ private:
 
 class CallableParts : public PartsBase {
 public:
-    bool call(std::vector<Variable>& args) {
-        auto [guard, pcm] = getWriter();
-        return (*pcm)[pcm->getFocusedPipeline()].call(args);
-    }
+    inline bool call(std::vector<Variable>& args);
 
-    bool call(const std::string& pipeline_name, std::vector<Variable>& args) {
-        auto [guard, pcm] = getWriter();
-        return (*pcm)[pipeline_name].call(args);
-    }
+    inline bool call(const std::string& pipeline_name,
+                     std::vector<Variable>& args);
 };
 
 template <typename... ReturnTypes>
 class HardCallableParts : public PartsBase {
 public:
     template <typename... Args>
-    std::tuple<ReturnTypes...> callHard(Args&&... args) {
-        struct Dum {
-            HardCallableParts<ReturnTypes...>* that;
-            void                               reset() {}
-            bool operator()(std::vector<Variable>& vs) {
-                auto [guard, pcm] = that->getWriter();
-                return (*pcm)[pcm->getFocusedPipeline()].call(vs);
-            }
-        };
-        return ToHard<ReturnTypes...>::template Pipe<Args...>::Gen(Dum{this})(
-                std::forward<Args>(args)...);
-    }
+    inline std::tuple<ReturnTypes...> callHard(Args&&... args);
 };
+
+}  // namespace fase
+
+// =============================================================================
+// =========================== Non User Interface ==============================
+// =============================================================================
+
+namespace fase {
+
+inline ExportedPipe ExportableParts ::exportPipe() const {
+    auto [guard, pcm] = getReader();
+    return pcm->exportPipe(pcm->getFocusedPipeline());
+}
+
+inline bool CallableParts::call(std::vector<Variable>& args) {
+    auto [guard, pcm] = getWriter();
+    return (*pcm)[pcm->getFocusedPipeline()].call(args);
+}
+
+inline bool CallableParts::call(const std::string& pipeline_name,
+                                std::vector<Variable>& args) {
+    auto [guard, pcm] = getWriter();
+    return (*pcm)[pipeline_name].call(args);
+}
+
+template <typename... RetTypes>
+template <typename... Args>
+template <class Exported>
+inline auto ToHard<RetTypes...>::Pipe<Args...>::Gen(Exported&& exported) {
+    class Dst {
+    public:
+        std::tuple<RetTypes...> operator()(Args... args) {
+            std::vector<Variable> arg_vs = {
+                    std::make_unique<std::decay_t<Args>>(
+                            std::forward<Args>(args))...};
+            [[maybe_unused]] auto _ = {
+                    arg_vs.emplace_back(typeid(RetTypes))...};
+            if (!soft(arg_vs)) {
+                throw std::runtime_error(
+                        "HardExportPipe : input/output type isn't "
+                        "match!");
+            }
+            std::vector<Variable*> ret_ps;
+            for (std::size_t i = arg_vs.size() - sizeof...(RetTypes);
+                 i < arg_vs.size(); i++) {
+                ret_ps.emplace_back(&arg_vs[i]);
+            }
+            return Wrap(ret_ps, std::index_sequence_for<RetTypes...>());
+        }
+        void reset() {
+            soft.reset();
+        }
+        std::decay_t<Exported> soft;
+    };
+    return Dst{std::forward<Exported>(exported)};
+}
+
+template <typename... ReturnTypes>
+template <typename... Args>
+inline std::tuple<ReturnTypes...> HardCallableParts<ReturnTypes...>::callHard(
+        Args&&... args) {
+    struct Dum {
+        HardCallableParts<ReturnTypes...>* that;
+        void reset() {}
+        bool operator()(std::vector<Variable>& vs) {
+            auto [guard, pcm] = that->getWriter();
+            return (*pcm)[pcm->getFocusedPipeline()].call(vs);
+        }
+    };
+    return ToHard<ReturnTypes...>::template Pipe<Args...>::Gen(Dum{this})(
+            std::forward<Args>(args)...);
+}
 
 }  // namespace fase
 
