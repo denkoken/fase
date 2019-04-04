1e6cab085062692153a5b73b7a813bfc0545faae
skatto <skattomaskatto@gmail.com>
2019-04-04 11:19:26 +0900

    change univ_function using mutable lambda.

--- src/fase2/univ_functions.h
+++ src/fase2/univ_functions.h
@@ -22,14 +22,11 @@ private:
     static auto ToUniv(Callable&& f, std::index_sequence<Seq...>) {
         using Callable_N = std::decay_t<Callable>;
         if constexpr (std::is_copy_constructible_v<Callable_N>) {
-            struct Dst {
-                Callable_N f;
-                void       operator()(std::vector<Variable>& args) {
-                    f(static_cast<Args>(
-                            *args[Seq].getWriter<std::decay_t<Args>>())...);
-                }
+            return [f = std::forward<Callable>(f)](
+                           std::vector<Variable>& args) mutable {
+                f(static_cast<Args>(
+                        *args[Seq].getWriter<std::decay_t<Args>>())...);
             };
-            return Dst{f};
         } else {
             auto fp = std::make_shared<Callable_N>(std::forward<Callable_N>(f));
             return [fp](std::vector<Variable>& args) {
@@ -41,24 +38,21 @@ private:
 
     template <typename Callable>
     static auto Wrap(Callable f) {
-        struct Dst {
-            Callable f;
-            void     operator()(std::vector<Variable>& args, Report* preport) {
-                if (args.size() != sizeof...(Args)) {
-                    throw std::logic_error(
-                            "Invalid size of variables at UnivFunc.");
-                }
-                if (preport != nullptr) {
-                    using std::chrono::system_clock;
-                    auto start_t = system_clock::now();
-                    f(args);
-                    preport->execution_time = system_clock::now() - start_t;
-                } else {
-                    f(args);
-                }
+        return [f = std::move(f)](std::vector<Variable>& args,
+                                  Report*                preport) mutable {
+            if (args.size() != sizeof...(Args)) {
+                throw std::logic_error(
+                        "Invalid size of variables at UnivFunc.");
+            }
+            if (preport != nullptr) {
+                using std::chrono::system_clock;
+                auto start_t = system_clock::now();
+                f(args);
+                preport->execution_time = system_clock::now() - start_t;
+            } else {
+                f(args);
             }
         };
-        return Dst{f};
     }
 };
 
